d12d8e85e06af26a9b5a833b9a41b1700f257e17
PureCloud Jenkins <purecloud-jenkins@ininica.com>
2025-02-21 17:21:39 +0000

    124.0.0

--- build/gc/cmd/users_externalid/users_externalid.go
+++ build/gc/cmd/users_externalid/users_externalid.go
@@ -14,7 +14,7 @@ import (
 )
 
 var (
-	Description = utils.FormatUsageDescription("users_externalid", "SWAGGER_OVERRIDE_/api/v2/users/{userId}/externalid", "SWAGGER_OVERRIDE_/api/v2/users/{userId}/externalid/{authorityName}", "SWAGGER_OVERRIDE_/api/v2/users/externalid/{authorityName}", "SWAGGER_OVERRIDE_/api/v2/users/{userId}/externalid", "SWAGGER_OVERRIDE_/api/v2/users/{userId}/externalid", )
+	Description = utils.FormatUsageDescription("users_externalid", "SWAGGER_OVERRIDE_/api/v2/users/{userId}/externalid", "SWAGGER_OVERRIDE_/api/v2/users/{userId}/externalid/{authorityName}", "SWAGGER_OVERRIDE_/api/v2/users/{userId}/externalid", "SWAGGER_OVERRIDE_/api/v2/users/{userId}/externalid", "SWAGGER_OVERRIDE_/api/v2/users/externalid/{authorityName}", )
 	users_externalidCmd = &cobra.Command{
 		Use:   utils.FormatUsageDescription("users_externalid"),
 		Short: Description,
@@ -64,26 +64,10 @@ func Cmdusers_externalid() *cobra.Command {
 }`)
 	users_externalidCmd.AddCommand(deleteCmd)
 
-	utils.AddFlag(getCmd.Flags(), "[]string", "expand", "", "Which fields, if any, to expand Valid values: routingStatus, presence, integrationPresence, conversationSummary, outOfOffice, geolocation, station, authorization, lasttokenissued, authorization.unusedRoles, team, workPlanBidRanks, externalContactsSettings, profileSkills, certifications, locations, groups, skills, languages, languagePreference, employerInfo, biography, dateLastLogin")
-	getCmd.SetUsageTemplate(fmt.Sprintf("%s\nOperation:\n  %s %s\n%s\n%s", getCmd.UsageTemplate(), "GET", "/api/v2/users/externalid/{authorityName}/{externalKey}", utils.FormatPermissions([]string{  }), utils.GenerateDevCentreLink("GET", "Users", "/api/v2/users/externalid/{authorityName}/{externalKey}")))
-	utils.AddFileFlagIfUpsert(getCmd.Flags(), "GET", ``)
+	getexternalidforuserauthorityCmd.SetUsageTemplate(fmt.Sprintf("%s\nOperation:\n  %s %s\n%s\n%s", getexternalidforuserauthorityCmd.UsageTemplate(), "GET", "/api/v2/users/{userId}/externalid/{authorityName}", utils.FormatPermissions([]string{  }), utils.GenerateDevCentreLink("GET", "Users", "/api/v2/users/{userId}/externalid/{authorityName}")))
+	utils.AddFileFlagIfUpsert(getexternalidforuserauthorityCmd.Flags(), "GET", ``)
 	
-	utils.AddPaginateFlagsIfListingResponse(getCmd.Flags(), "GET", `{
-  "description" : "successful operation",
-  "content" : {
-    "application/json" : {
-      "schema" : {
-        "$ref" : "#/components/schemas/User"
-      }
-    }
-  }
-}`)
-	users_externalidCmd.AddCommand(getCmd)
-
-	getCmd.SetUsageTemplate(fmt.Sprintf("%s\nOperation:\n  %s %s\n%s\n%s", getCmd.UsageTemplate(), "GET", "/api/v2/users/{userId}/externalid/{authorityName}", utils.FormatPermissions([]string{  }), utils.GenerateDevCentreLink("GET", "Users", "/api/v2/users/{userId}/externalid/{authorityName}")))
-	utils.AddFileFlagIfUpsert(getCmd.Flags(), "GET", ``)
-	
-	utils.AddPaginateFlagsIfListingResponse(getCmd.Flags(), "GET", `{
+	utils.AddPaginateFlagsIfListingResponse(getexternalidforuserauthorityCmd.Flags(), "GET", `{
   "description" : "successful operation",
   "content" : {
     "application/json" : {
@@ -93,12 +77,12 @@ func Cmdusers_externalid() *cobra.Command {
     }
   }
 }`)
-	users_externalidCmd.AddCommand(getCmd)
+	users_externalidCmd.AddCommand(getexternalidforuserauthorityCmd)
 
-	getCmd.SetUsageTemplate(fmt.Sprintf("%s\nOperation:\n  %s %s\n%s\n%s", getCmd.UsageTemplate(), "GET", "/api/v2/users/{userId}/externalid", utils.FormatPermissions([]string{  }), utils.GenerateDevCentreLink("GET", "Users", "/api/v2/users/{userId}/externalid")))
-	utils.AddFileFlagIfUpsert(getCmd.Flags(), "GET", ``)
+	getexternalidsforuserCmd.SetUsageTemplate(fmt.Sprintf("%s\nOperation:\n  %s %s\n%s\n%s", getexternalidsforuserCmd.UsageTemplate(), "GET", "/api/v2/users/{userId}/externalid", utils.FormatPermissions([]string{  }), utils.GenerateDevCentreLink("GET", "Users", "/api/v2/users/{userId}/externalid")))
+	utils.AddFileFlagIfUpsert(getexternalidsforuserCmd.Flags(), "GET", ``)
 	
-	utils.AddPaginateFlagsIfListingResponse(getCmd.Flags(), "GET", `{
+	utils.AddPaginateFlagsIfListingResponse(getexternalidsforuserCmd.Flags(), "GET", `{
   "description" : "successful operation",
   "content" : {
     "application/json" : {
@@ -111,7 +95,23 @@ func Cmdusers_externalid() *cobra.Command {
     }
   }
 }`)
-	users_externalidCmd.AddCommand(getCmd)
+	users_externalidCmd.AddCommand(getexternalidsforuserCmd)
+
+	utils.AddFlag(getuserbyexternalidCmd.Flags(), "[]string", "expand", "", "Which fields, if any, to expand Valid values: routingStatus, presence, integrationPresence, conversationSummary, outOfOffice, geolocation, station, authorization, lasttokenissued, authorization.unusedRoles, team, workPlanBidRanks, externalContactsSettings, profileSkills, certifications, locations, groups, skills, languages, languagePreference, employerInfo, biography, dateLastLogin")
+	getuserbyexternalidCmd.SetUsageTemplate(fmt.Sprintf("%s\nOperation:\n  %s %s\n%s\n%s", getuserbyexternalidCmd.UsageTemplate(), "GET", "/api/v2/users/externalid/{authorityName}/{externalKey}", utils.FormatPermissions([]string{  }), utils.GenerateDevCentreLink("GET", "Users", "/api/v2/users/externalid/{authorityName}/{externalKey}")))
+	utils.AddFileFlagIfUpsert(getuserbyexternalidCmd.Flags(), "GET", ``)
+	
+	utils.AddPaginateFlagsIfListingResponse(getuserbyexternalidCmd.Flags(), "GET", `{
+  "description" : "successful operation",
+  "content" : {
+    "application/json" : {
+      "schema" : {
+        "$ref" : "#/components/schemas/User"
+      }
+    }
+  }
+}`)
+	users_externalidCmd.AddCommand(getuserbyexternalidCmd)
 	return users_externalidCmd
 }
 
@@ -257,8 +257,140 @@ var deleteCmd = &cobra.Command{
 		utils.Render(results)
 	},
 }
-var getCmd = &cobra.Command{
-	Use:   "get [authorityName] [externalKey]",
+var getexternalidforuserauthorityCmd = &cobra.Command{
+	Use:   "getexternalidforuserauthority [userId] [authorityName]",
+	Short: "Get the external identifier of user for an authority.",
+	Long:  "Get the external identifier of user for an authority.",
+	Args:  utils.DetermineArgs([]string{ "userId", "authorityName", }),
+
+	Run: func(cmd *cobra.Command, args []string) {
+		_ = models.Entities{}
+
+		printReqBody, _ := cmd.Flags().GetBool("printrequestbody")
+		if printReqBody {
+			
+			return
+		}
+
+		queryParams := make(map[string]string)
+
+		path := "/api/v2/users/{userId}/externalid/{authorityName}"
+		userId, args := args[0], args[1:]
+		path = strings.Replace(path, "{userId}", fmt.Sprintf("%v", userId), -1)
+		authorityName, args := args[0], args[1:]
+		path = strings.Replace(path, "{authorityName}", fmt.Sprintf("%v", authorityName), -1)
+
+		urlString := path
+		if len(queryParams) > 0 {
+			urlString = fmt.Sprintf("%v?", path)
+			for k, v := range queryParams {
+				urlString += fmt.Sprintf("%v=%v&", queryEscape(strings.TrimSpace(k)), queryEscape(strings.TrimSpace(v)))
+			}
+			urlString = strings.TrimSuffix(urlString, "&")
+		}
+
+		if strings.Contains(urlString, "varType") {
+			urlString = strings.Replace(urlString, "varType", "type", -1)
+		}
+
+		const opId = "getexternalidforuserauthority"
+		const httpMethod = "GET"
+		retryFunc := CommandService.DetermineAction(httpMethod, urlString, cmd, opId)
+		// TODO read from config file
+		retryConfig := &retry.RetryConfiguration{
+			RetryWaitMin: 5 * time.Second,
+			RetryWaitMax: 60 * time.Second,
+			RetryMax:     20,
+		}
+		results, err := retryFunc(retryConfig)
+		if err != nil {
+			if httpMethod == "HEAD" {
+				if httpErr, ok := err.(models.HttpStatusError); ok {
+					logger.Fatal(fmt.Sprintf("Status Code %v\n", httpErr.StatusCode))
+				}
+			}
+			logger.Fatal(err)
+		}
+
+		filterCondition, _ := cmd.Flags().GetString("filtercondition")
+		if filterCondition != "" {
+			filteredResults, err := utils.FilterByCondition(results, filterCondition)
+			if err != nil {
+				logger.Fatal(err)
+			}
+			results = filteredResults
+		}
+
+		utils.Render(results)
+	},
+}
+var getexternalidsforuserCmd = &cobra.Command{
+	Use:   "getexternalidsforuser [userId]",
+	Short: "Get the external identifiers for a user.",
+	Long:  "Get the external identifiers for a user.",
+	Args:  utils.DetermineArgs([]string{ "userId", }),
+
+	Run: func(cmd *cobra.Command, args []string) {
+		_ = models.Entities{}
+
+		printReqBody, _ := cmd.Flags().GetBool("printrequestbody")
+		if printReqBody {
+			
+			return
+		}
+
+		queryParams := make(map[string]string)
+
+		path := "/api/v2/users/{userId}/externalid"
+		userId, args := args[0], args[1:]
+		path = strings.Replace(path, "{userId}", fmt.Sprintf("%v", userId), -1)
+
+		urlString := path
+		if len(queryParams) > 0 {
+			urlString = fmt.Sprintf("%v?", path)
+			for k, v := range queryParams {
+				urlString += fmt.Sprintf("%v=%v&", queryEscape(strings.TrimSpace(k)), queryEscape(strings.TrimSpace(v)))
+			}
+			urlString = strings.TrimSuffix(urlString, "&")
+		}
+
+		if strings.Contains(urlString, "varType") {
+			urlString = strings.Replace(urlString, "varType", "type", -1)
+		}
+
+		const opId = "getexternalidsforuser"
+		const httpMethod = "GET"
+		retryFunc := CommandService.DetermineAction(httpMethod, urlString, cmd, opId)
+		// TODO read from config file
+		retryConfig := &retry.RetryConfiguration{
+			RetryWaitMin: 5 * time.Second,
+			RetryWaitMax: 60 * time.Second,
+			RetryMax:     20,
+		}
+		results, err := retryFunc(retryConfig)
+		if err != nil {
+			if httpMethod == "HEAD" {
+				if httpErr, ok := err.(models.HttpStatusError); ok {
+					logger.Fatal(fmt.Sprintf("Status Code %v\n", httpErr.StatusCode))
+				}
+			}
+			logger.Fatal(err)
+		}
+
+		filterCondition, _ := cmd.Flags().GetString("filtercondition")
+		if filterCondition != "" {
+			filteredResults, err := utils.FilterByCondition(results, filterCondition)
+			if err != nil {
+				logger.Fatal(err)
+			}
+			results = filteredResults
+		}
+
+		utils.Render(results)
+	},
+}
+var getuserbyexternalidCmd = &cobra.Command{
+	Use:   "getuserbyexternalid [authorityName] [externalKey]",
 	Short: "Get the user associated with external identifier.",
 	Long:  "Get the user associated with external identifier.",
 	Args:  utils.DetermineArgs([]string{ "authorityName", "externalKey", }),
@@ -297,139 +429,7 @@ var getCmd = &cobra.Command{
 			urlString = strings.Replace(urlString, "varType", "type", -1)
 		}
 
-		const opId = "get"
-		const httpMethod = "GET"
-		retryFunc := CommandService.DetermineAction(httpMethod, urlString, cmd, opId)
-		// TODO read from config file
-		retryConfig := &retry.RetryConfiguration{
-			RetryWaitMin: 5 * time.Second,
-			RetryWaitMax: 60 * time.Second,
-			RetryMax:     20,
-		}
-		results, err := retryFunc(retryConfig)
-		if err != nil {
-			if httpMethod == "HEAD" {
-				if httpErr, ok := err.(models.HttpStatusError); ok {
-					logger.Fatal(fmt.Sprintf("Status Code %v\n", httpErr.StatusCode))
-				}
-			}
-			logger.Fatal(err)
-		}
-
-		filterCondition, _ := cmd.Flags().GetString("filtercondition")
-		if filterCondition != "" {
-			filteredResults, err := utils.FilterByCondition(results, filterCondition)
-			if err != nil {
-				logger.Fatal(err)
-			}
-			results = filteredResults
-		}
-
-		utils.Render(results)
-	},
-}
-var getCmd = &cobra.Command{
-	Use:   "get [userId] [authorityName]",
-	Short: "Get the external identifier of user for an authority.",
-	Long:  "Get the external identifier of user for an authority.",
-	Args:  utils.DetermineArgs([]string{ "userId", "authorityName", }),
-
-	Run: func(cmd *cobra.Command, args []string) {
-		_ = models.Entities{}
-
-		printReqBody, _ := cmd.Flags().GetBool("printrequestbody")
-		if printReqBody {
-			
-			return
-		}
-
-		queryParams := make(map[string]string)
-
-		path := "/api/v2/users/{userId}/externalid/{authorityName}"
-		userId, args := args[0], args[1:]
-		path = strings.Replace(path, "{userId}", fmt.Sprintf("%v", userId), -1)
-		authorityName, args := args[0], args[1:]
-		path = strings.Replace(path, "{authorityName}", fmt.Sprintf("%v", authorityName), -1)
-
-		urlString := path
-		if len(queryParams) > 0 {
-			urlString = fmt.Sprintf("%v?", path)
-			for k, v := range queryParams {
-				urlString += fmt.Sprintf("%v=%v&", queryEscape(strings.TrimSpace(k)), queryEscape(strings.TrimSpace(v)))
-			}
-			urlString = strings.TrimSuffix(urlString, "&")
-		}
-
-		if strings.Contains(urlString, "varType") {
-			urlString = strings.Replace(urlString, "varType", "type", -1)
-		}
-
-		const opId = "get"
-		const httpMethod = "GET"
-		retryFunc := CommandService.DetermineAction(httpMethod, urlString, cmd, opId)
-		// TODO read from config file
-		retryConfig := &retry.RetryConfiguration{
-			RetryWaitMin: 5 * time.Second,
-			RetryWaitMax: 60 * time.Second,
-			RetryMax:     20,
-		}
-		results, err := retryFunc(retryConfig)
-		if err != nil {
-			if httpMethod == "HEAD" {
-				if httpErr, ok := err.(models.HttpStatusError); ok {
-					logger.Fatal(fmt.Sprintf("Status Code %v\n", httpErr.StatusCode))
-				}
-			}
-			logger.Fatal(err)
-		}
-
-		filterCondition, _ := cmd.Flags().GetString("filtercondition")
-		if filterCondition != "" {
-			filteredResults, err := utils.FilterByCondition(results, filterCondition)
-			if err != nil {
-				logger.Fatal(err)
-			}
-			results = filteredResults
-		}
-
-		utils.Render(results)
-	},
-}
-var getCmd = &cobra.Command{
-	Use:   "get [userId]",
-	Short: "Get the external identifiers for a user.",
-	Long:  "Get the external identifiers for a user.",
-	Args:  utils.DetermineArgs([]string{ "userId", }),
-
-	Run: func(cmd *cobra.Command, args []string) {
-		_ = models.Entities{}
-
-		printReqBody, _ := cmd.Flags().GetBool("printrequestbody")
-		if printReqBody {
-			
-			return
-		}
-
-		queryParams := make(map[string]string)
-
-		path := "/api/v2/users/{userId}/externalid"
-		userId, args := args[0], args[1:]
-		path = strings.Replace(path, "{userId}", fmt.Sprintf("%v", userId), -1)
-
-		urlString := path
-		if len(queryParams) > 0 {
-			urlString = fmt.Sprintf("%v?", path)
-			for k, v := range queryParams {
-				urlString += fmt.Sprintf("%v=%v&", queryEscape(strings.TrimSpace(k)), queryEscape(strings.TrimSpace(v)))
-			}
-			urlString = strings.TrimSuffix(urlString, "&")
-		}
-
-		if strings.Contains(urlString, "varType") {
-			urlString = strings.Replace(urlString, "varType", "type", -1)
-		}
-
-		const opId = "get"
+		const opId = "getuserbyexternalid"
 		const httpMethod = "GET"
 		retryFunc := CommandService.DetermineAction(httpMethod, urlString, cmd, opId)
 		// TODO read from config file
--- build/gc/go.mod
+++ build/gc/go.mod
@@ -6,44 +6,43 @@ require (
 	github.com/Masterminds/sprig v2.22.0+incompatible
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/websocket v1.5.3
-	github.com/hashicorp/go-retryablehttp v0.7.5
+	github.com/hashicorp/go-retryablehttp v0.7.7
 	github.com/hashicorp/go-version v1.7.0
-	github.com/spf13/cobra v1.8.0
-	github.com/spf13/pflag v1.0.5
+	github.com/spf13/cobra v1.9.1
+	github.com/spf13/pflag v1.0.6
 	github.com/spf13/viper v1.19.0
 	github.com/tidwall/pretty v1.2.1
-	golang.org/x/term v0.24.0
+	golang.org/x/term v0.29.0
 	sigs.k8s.io/yaml v1.4.0
 )
 
 require (
 	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/semver v1.5.0 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
-	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.6 // indirect
+	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/huandu/xstrings v1.3.2 // indirect
-	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/huandu/xstrings v1.5.0 // indirect
+	github.com/imdario/mergo v0.3.16 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/magiconair/properties v1.8.9 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
-	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/sagikazarmark/locafero v0.4.0 // indirect
+	github.com/sagikazarmark/locafero v0.7.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
-	github.com/spf13/afero v1.11.0 // indirect
-	github.com/spf13/cast v1.6.0 // indirect
+	github.com/spf13/afero v1.12.0 // indirect
+	github.com/spf13/cast v1.7.1 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
-	go.uber.org/atomic v1.9.0 // indirect
-	go.uber.org/multierr v1.9.0 // indirect
-	golang.org/x/crypto v0.21.0 // indirect
-	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/sys v0.25.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/exp v0.0.0-20250218142911-aa4b98e5adaa // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )

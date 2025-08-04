b7f5ae40c5ee60bb567da05e8c6cbf11e0ffe238
PureCloud Jenkins <purecloud-jenkins@ininica.com>
2025-08-04 18:17:07 +0000

    136.0.0

--- build/gc/cmd/root.go
+++ build/gc/cmd/root.go
@@ -48,7 +48,7 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of gc",
 	Long:  `All software has versions. This is gc version's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Current version: 135.0.0")
+		fmt.Println("Current version: 136.0.0")
 		checkForNewVersion()
 	},
 }
@@ -67,7 +67,7 @@ func checkForNewVersion() {
 		return
 	}
 
-	if versionsAreEqual("135.0.0", latestVersion) {
+	if versionsAreEqual("136.0.0", latestVersion) {
 		fmt.Println("You're all up to date.")
 	} else {
 		fmt.Printf("A new version of the CLI is available: %v\n", latestVersion)
